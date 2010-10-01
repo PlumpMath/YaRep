93a73098a4b0b6979745beca2041b0f8be8e3cb9
Mikhail Polovnev <lih.meh@gmail.com>
2010-10-01 12:56:46 +0400

    Первая рабочая версия

--- YaRep/Report.cs
+++ YaRep/Report.cs
@@ -26,11 +26,30 @@ namespace YaRep
             return new Report();
         }
 
+        private string PrepareSheetNameForExcel(string sheetName)
+        {
+            return sheetName.Replace('.', '#');
+        }
+
         /// <summary>
         /// Вызывается в конце работы над отчетом. В этот момент данные непосредственно выгружаются в excel и книга показывается пользователю.
         /// </summary>
         public void End()
         {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = false;
+            var workbook = excel.Workbooks.Add(Type.Missing);
+            foreach (var sheet in sheets)
+            {
+                var excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets.Add(Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                excelSheet.Name = PrepareSheetNameForExcel(sheet.Key);
+                var data = sheet.Value.GetArray();
+                int cols = data.GetLength(0);
+                int rows = data.GetLength(0);
+                if ((cols < 1) || (rows < 1)) continue;
+                excelSheet.get_Range(excelSheet.Cells[1, 1], excelSheet.Cells[rows, cols]).Value2 = data;
+            }
+            excel.Visible = true;
         }
 
         private Dictionary<string,Sheet> sheets = new Dictionary<string,Sheet>();
